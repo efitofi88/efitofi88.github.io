39787e2ca1835b7662a9d9b37c0f80018fbf4d56
efitofi88 <61872223+efitofi88@users.noreply.github.com>
2020-03-06 22:21:16 +0300

    Add files via upload

---
+++ precache-manifest.7db8bcef5bf4bec3e1129cbeb957a46d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c99dfef3b5708955e3ec7d434ff54009",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a72347eb9a29ade6a8e7",
+    "url": "./static/css/2.ae2f4c93.chunk.css"
+  },
+  {
+    "revision": "ba8aab157fde0883452d",
+    "url": "./static/css/main.cbaedcda.chunk.css"
+  },
+  {
+    "revision": "a72347eb9a29ade6a8e7",
+    "url": "./static/js/2.5c5d15ca.chunk.js"
+  },
+  {
+    "revision": "ba8aab157fde0883452d",
+    "url": "./static/js/main.5705a5b9.chunk.js"
+  },
+  {
+    "revision": "fd7ef9e7f1f5d35e6ba9",
+    "url": "./static/js/runtime-main.82b5115b.js"
+  },
+  {
+    "revision": "36ec06394b9c159b44e3384ae610d353",
+    "url": "./static/media/img.36ec0639.png"
+  }
+]);
